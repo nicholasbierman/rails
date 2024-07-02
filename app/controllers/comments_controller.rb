07fc5e73a16bb49ca90b5b7c93955cbcf683a960
Nick Bierman <biermannick83@gmail.com>
2024-07-02 14:07:25 -0500

    add destroy method to CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,18 @@
 class CommentsController < ApplicationController
 
-  def create
+  def find_parent_article
     @article = Article.find(params[:article_id])
-    @comments = @article.comments.create(comment_params)
+  end
+
+  def create
+    @comments = find_parent_article.comments.create(comment_params)
     redirect_to article_path(@article)
+  end
+
+  def destroy
+    @comment = find_parent_article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other
   end
 
   private
